4f2454f2bc73ffd9020044abc6f3678eac9e1ee3
smk <mathieusalhi@yahoo.fr>
2025-06-14 18:47:17 +0200

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "input.hpp"
+
+void input(std::string& entrer){
+	std::getline(std::cin, entrer);
+
+}
+
+
---
+++ input.hpp
@@ -0,0 +1,8 @@
+#ifndef INPUT_HPP
+#define INPUT_CPP
+
+#include <string>
+
+void input(std::string& reponse);
+
+#endif //INPUT_HPP
